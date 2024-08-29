33d9cf4b6480d20ff4c48a877e1859a0ce5b6809
Roberto Antônio <74279517+robertoantony32@users.noreply.github.com>
2024-08-28 22:47:20 -0300

    Update: adding url to header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,10 @@ export function Header() {
     <header className="min-w-[150px] h-12 flex justify-center mb-10 lg:mb-0 lg:justify-start">
       <nav className="flex gap-4 bg-white px-4 lg:px-4 h-12 items-center rounded-xl fixed">
         {[
-          { name: "Github", imgLink: "icons/github.png" },
-          { name: "Linkedin", imgLink: "icons/linkedin.svg" },
+          { name: "Github", imgLink: "icons/github.png", link: "https://github.com/robertoantony32" },
+          { name: "Linkedin", imgLink: "icons/linkedin.svg", link: "https://www.linkedin.com/in/roberto-antônio/" },
         ].map((item) => (
-          <a key={item.name} className="size-8" href="#">
+          <a key={item.name} className="size-8" href={item.link}>
             <img src={item.imgLink} alt={item.name} />
           </a>
         ))}
